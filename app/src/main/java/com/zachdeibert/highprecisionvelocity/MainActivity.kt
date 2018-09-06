e973674ab1ba93d71e26ed08c74cc5fca2873b0d
Zach Deibert <zachdeibert@gmail.com>
2018-09-06 15:21:57 -0500

    Added settings activity

--- app/src/main/java/com/zachdeibert/highprecisionvelocity/MainActivity.kt
+++ app/src/main/java/com/zachdeibert/highprecisionvelocity/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.zachdeibert.highprecisionvelocity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.ViewPager
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<ViewPager>(R.id.viewPager).adapter = MainPagerAdapter(supportFragmentManager)
+    }
+
+    fun openSettings(view: View) {
+        startActivity(Intent(this, SettingsActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/zachdeibert/highprecisionvelocity/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.zachdeibert.highprecisionvelocity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
